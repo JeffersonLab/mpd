54a83e69e6cc2c0acca9d41b5ccf75f8b5fa08a6
Bryan Moffit <moffit@jlab.org>
2018-09-24 15:49:38 -0400

    Updated TCL script for setting randy_factor

--- libsrc/rol/setrf.tcl
+++ libsrc/rol/setrf.tcl
@@ -4,25 +4,45 @@
 #  You must include the following arguments
 #     val      - value to set randy_factor to
 #
-#
 #  To include this function in the coda_roc at boot
 #
 #     coda_roc -i -n ROC1 -t ROC -f setrf.tcl
 #
 
-proc setrf {val}
-{
+proc setrf {val} {
     global env
 
     set rocname [info objects]
     $rocname configure -randy_factor $val
 
     set myvar [$rocname cget -randy_factor]
-
-    puts "setrf -> $myvar"
+    puts ""
+    puts "************** setrf.tcl ***************"
+    puts "*"
+    puts "* Max Events per output Buffer = $myvar"
+    puts "*"
+    puts "************** setrf.tcl ***************"
+    puts ""
 
     return $myvar
 }
 
+proc getrf {} {
+    global env
 
-setrf 50
+    set rocname [info objects]
+
+    set myvar [$rocname cget -randy_factor]
+    puts ""
+    puts "************** setrf.tcl ***************"
+    puts "*"
+    puts "* Max Events per output Buffer = $myvar"
+    puts "*"
+    puts "************** setrf.tcl ***************"
+    puts ""
+
+    return $myvar
+}
+
+# Set when this script is loaded by the ROC with "-f setrf.tcl"
+setrf 5
