560791e65460c17eb1210e4651b7d67cd3b9438e
Bryan Moffit <moffit@jlab.org>
2025-01-15 13:04:58 -0500

    fix the units of rx and txpower in transceiver status

--- libsrc/mpdLib.c
+++ libsrc/mpdLib.c
@@ -6414,7 +6414,7 @@ mpdTransceiverGStatus()
 
   printf("                      MPD Transceiver Status\n\n");
   printf("                             ....... TX .......     RX     Alarms    Warnings\n");
-  printf("Slot     Temp[C]   Vcc[V]    Bias[mA]  Power[mW] Power[mW] TVBPR     TVBPR\n");
+  printf("Slot     Temp[C]   Vcc[V]    Bias[mA]  Power[uW] Power[uW] TVBPR     TVBPR\n");
   printf("--------------------------------------------------------------------------------\n");
 
   int32_t id, impd;
@@ -6466,11 +6466,11 @@ mpdTransceiverGStatus()
 
       printf("  %2d     ", id);
 
-      printf("%5.1f     ", ((float) temp) / 256.);
-      printf("%5.2f     ", ((float) vcc) * 100. / 1000000.);
-      printf("%5.2f     ", ((float) txbias) * 2. / 1000.);
-      printf("%5.1f     ", ((float) txpower) * 10. / 1000);
-      printf("%5.1f     ", ((float) rxpower) * 10. / 1000);
+      printf("%5.1f     ", ((float) temp) / 256.); /* [C] / 256 */
+      printf("%5.2f     ", ((float) vcc) * 100. / 1000000.); /* 100 [uV] */
+      printf("%5.2f     ", ((float) txbias) * 2. / 1000.);   /* 2 [uA] */
+      printf("%7.1f   ", ((float) txpower) * 0.100);  /* 0.1 [uW] */
+      printf("%7.1f   ", ((float) rxpower) * 0.100);  /* 0.1 [uW] */
       printf("%s%s%s%s%s     ",
 	     (alarms[0] & (1<<7)) ? "H" : (alarms[0] & (1<<6)) ? "L" : "-",
 	     (alarms[0] & (1<<5)) ? "H" : (alarms[0] & (1<<4)) ? "L" : "-",
