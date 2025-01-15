7bb032c9fe574c3df5341c35e3006af3397b3cd7
Bryan Moffit <moffit@jlab.org>
2025-01-14 22:52:56 -0500

    mpdTransceiverStatus to check transceiver i2c
    - use local davme7.cfg file
    - print out data from other onboard i2c devices
    - printout result of i2c byte read from address 0xa2

---
+++ libsrc/test/mpdTransceiverStatus.c
@@ -0,0 +1,112 @@
+/*
+ * File:
+ *    mpdTransceiverStatus.c
+ *
+ * Description:
+ *    Attempt to use i2c core to communicate with transceiver
+ *    addresses with i2c
+ *
+ */
+
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "jvme.h"
+#include "mpdLib.h"
+#include "mpdConfig.h"
+
+int
+main(int argc, char *argv[])
+{
+  int stat, slot;
+  char filename[255] = "/daqfs/daq_setups/mpd_transceiver_status/cfg/davme7.cfg";
+
+
+  if (argc > 1)
+    {
+      slot = atoi(argv[1]);
+
+      if ((slot < 0) || (slot > 32))
+	{
+	  printf("invalid slot... using 21");
+	  slot = 2;
+	}
+      if(argc > 2)
+	{
+	  strncpy(filename, argv[2], 255);
+	}
+    }
+  else
+    slot = 2;
+
+  printf("\n %s: slot = %d\n", argv[0], slot);
+  printf("----------------------------\n");
+
+  stat = vmeOpenDefaultWindows();
+  if(stat != OK)
+    goto CLOSE;
+
+  vmeCheckMutexHealth(1);
+  vmeBusLock();
+
+
+  if(mpdConfigInit(filename) < 0)
+    {
+      printf(" Config initialization ERROR!\n");
+      goto CLOSE;
+    }
+  mpdConfigLoad();
+
+  if(mpdInit((slot << 19), (1<<19), 1, 0) < 0)
+    {
+      printf("%s: Init error \n",
+	     __func__);
+      goto CLOSE;
+    }
+
+  slot = mpdSlot(0);
+  printf("MPD slot %2d config:\n", slot);
+
+  mpdFiberStatus(slot);
+
+  printf(" - Initialize I2C\n");
+
+  if(mpdI2C_Init(slot) != OK)
+    {
+      printf(" * * FAILED\n");
+    }
+
+  double core_t = 0.0, air_t = 0.0;
+  stat = mpdLM95235_Read(slot, &core_t, &air_t);
+
+  printf("Slot %2d - Board temperatures: core: %.2f degC air: %.2f degC   stat = %d\n",
+	 slot, core_t, air_t, stat);
+
+  mpdDELAY25_GStatus();
+
+  uint8_t i2c_addr = 0xA2, reg_addr = 0x30, val = 0;
+  stat = mpdI2C_ByteRead(slot, i2c_addr, reg_addr, 1, &val);
+
+  printf("  stat = %d  i2c_addr = 0x%02x  reg_addr = 0x%02x   val = 0x%02x\n", stat,
+	 i2c_addr, reg_addr, val);
+
+  mpdFiberEnable(slot);
+  printf(" --- Fiber Mode enabled ---\n");
+
+ CLOSE:
+  vmeBusUnlock();
+
+  vmeCloseDefaultWindows();
+
+  exit(0);
+
+}
+
+/*
+  Local Variables:
+  compile-command: "make -k mpdTransceiverStatus "
+  End:
+*/
